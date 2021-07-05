1a4337d23a593d62c757562d0311719f67126537
Chadi <jbara.chadi@outlook.fr>
2021-07-05 23:10:30 +0100

    v1.2

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -3,11 +3,9 @@ import { combineReducers } from "redux";
 import { itemsReducer } from "./items";
 import { inputReducer } from "./input";
 import { editReducer } from "./edit";
-import { refreshReducer } from "./refresh";
 
 export const allReducers = combineReducers({
     items: itemsReducer,
     text: inputReducer,
     edit: editReducer,
-    refresh: refreshReducer,
 });
